7fbac95ac72b80ede2b3a03205fa5c45adfacb4a
KubaGwozdz <jakub.gwozdz@icloud.com>
2020-11-08 16:17:45 +0100

    tumor growth model init

--- TumorGrowthModel.py
+++ TumorGrowthModel.py
@@ -1,4 +1,5 @@
 import _py_abc as pyabc
+from enum import Enum
 
 '''
 P - proliferative tissue, tumor tissue
@@ -12,7 +13,7 @@ gamma_P - damages in P tissue caused by the drug
 gamma_Q - damages in Q tissue caused by the drug
 k_QP_P - constant rate of transfer from QP to P tissue
 delta_QP - constant rate of elimination of QP tissue
-k_P_Q - constant rate of transfer from P to Q tissue
+k_PQ - constant rate of transfer from P to Q tissue
 K - maximal tumor size
 
 dCdt = -KDE * C
@@ -25,8 +26,24 @@ P' = P + Q + QP
 '''
 
 
+class Therapy(Enum):
+    TMZ = "temozolomide chemotherapy"
+    PCV = "1-(2-chloroethyl)-3-cyclohexyl-l-nitrosourea, and vincristine chemotherapy"
+    RT = "radiotherapy"
+
 
 class TumorGrowthModel:
+    def __init__(self, P0, Q0, lambda_P, k_PQ, k_QP_P, delta_QP, gamma, KDE):
+        self.P0 = P0
+        self.Q0 = Q0
+        self.lambda_P = lambda_P
+        self.k_PQ = k_PQ
+        self.k_QP_P = k_QP_P
+        self.delta_QP = delta_QP
+        self.gamma = gamma
+        self.KDE = KDE
 
-    def __init__(self, ):
 
+class TMZModel(TumorGrowthModel):
+    def __init__(self, P0, Q0, lambda_P, k_PQ, k_QP_P, delta_QP, gamma, KDE):
+        super.__init__(P0, Q0, lambda_P, k_PQ, k_QP_P, delta_QP, gamma, KDE)
